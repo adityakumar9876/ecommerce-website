d4bdf48fde4e09a9c65f2bffe4142cfe55e64dab
Aditya Kumar Manav <adityakumarmanav909@gmail.com>
2025-08-12 15:42:50 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,39 +1,21 @@
-# Nano ID
+# picocolors
 
-<img src="https://ai.github.io/nanoid/logo.svg" align="right"
-     alt="Nano ID logo by Anton Lovchikov" width="180" height="94">
+The tiniest and the fastest library for terminal output formatting with ANSI colors.
 
-**English** | [Русский](./README.ru.md) | [简体中文](./README.zh-CN.md) | [Bahasa Indonesia](./README.id-ID.md)
+```javascript
+import pc from "picocolors"
 
-A tiny, secure, URL-friendly, unique string ID generator for JavaScript.
-
-> “An amazing level of senseless perfectionism,
-> which is simply impossible not to respect.”
-
-* **Small.** 130 bytes (minified and gzipped). No dependencies.
-  [Size Limit] controls the size.
-* **Fast.** It is 2 times faster than UUID.
-* **Safe.** It uses hardware random generator. Can be used in clusters.
-* **Short IDs.** It uses a larger alphabet than UUID (`A-Za-z0-9_-`).
-  So ID size was reduced from 36 to 21 symbols.
-* **Portable.** Nano ID was ported
-  to [20 programming languages](#other-programming-languages).
-
-```js
-import { nanoid } from 'nanoid'
-model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
+console.log(
+  pc.green(`How are ${pc.italic(`you`)} doing?`)
+)
 ```
 
-Supports modern browsers, IE [with Babel], Node.js and React Native.
-
-[online tool]: https://gitpod.io/#https://github.com/ai/nanoid/
-[with Babel]:  https://developer.epages.com/blog/coding/how-to-transpile-node-modules-with-babel-and-webpack-in-a-monorepo/
-[Size Limit]:  https://github.com/ai/size-limit
-
-<a href="https://evilmartians.com/?utm_source=nanoid">
-  <img src="https://evilmartians.com/badges/sponsored-by-evil-martians.svg"
-       alt="Sponsored by Evil Martians" width="236" height="54">
-</a>
+- **No dependencies.**
+- **14 times** smaller and **2 times** faster than chalk.
+- Used by popular tools like PostCSS, SVGO, Stylelint, and Browserslist.
+- Node.js v6+ & browsers support. Support for both CJS and ESM projects.
+- TypeScript type declarations included.
+- [`NO_COLOR`](https://no-color.org/) friendly.
 
 ## Docs
-Read full docs **[here](https://github.com/ai/nanoid#readme)**.
+Read **[full docs](https://github.com/alexeyraspopov/picocolors#readme)** on GitHub.
--- types.d.ts
+++ types.d.ts
@@ -1,61 +1,51 @@
-/// <reference types="node" />
-import { Queue } from "./api/queue";
-export type Counts = {
-    files: number;
-    directories: number;
-    /**
-     * @deprecated use `directories` instead. Will be removed in v7.0.
-     */
-    dirs: number;
-};
-export type Group = {
-    directory: string;
-    files: string[];
-    /**
-     * @deprecated use `directory` instead. Will be removed in v7.0.
-     */
-    dir: string;
-};
-export type GroupOutput = Group[];
-export type OnlyCountsOutput = Counts;
-export type PathsOutput = string[];
-export type Output = OnlyCountsOutput | PathsOutput | GroupOutput;
-export type WalkerState = {
-    root: string;
-    paths: string[];
-    groups: Group[];
-    counts: Counts;
-    options: Options;
-    queue: Queue;
-    controller: AbortController;
-    symlinks: Map<string, string>;
-    visited: string[];
-};
-export type ResultCallback<TOutput extends Output> = (error: Error | null, output: TOutput) => void;
-export type FilterPredicate = (path: string, isDirectory: boolean) => boolean;
-export type ExcludePredicate = (dirName: string, dirPath: string) => boolean;
-export type PathSeparator = "/" | "\\";
-export type Options<TGlobFunction = unknown> = {
-    includeBasePath?: boolean;
-    includeDirs?: boolean;
-    normalizePath?: boolean;
-    maxDepth: number;
-    maxFiles?: number;
-    resolvePaths?: boolean;
-    suppressErrors: boolean;
-    group?: boolean;
-    onlyCounts?: boolean;
-    filters: FilterPredicate[];
-    resolveSymlinks?: boolean;
-    useRealPaths?: boolean;
-    excludeFiles?: boolean;
-    excludeSymlinks?: boolean;
-    exclude?: ExcludePredicate;
-    relativePaths?: boolean;
-    pathSeparator: PathSeparator;
-    signal?: AbortSignal;
-    globFunction?: TGlobFunction;
-};
-export type GlobMatcher = (test: string) => boolean;
-export type GlobFunction = (glob: string | string[], ...params: unknown[]) => GlobMatcher;
-export type GlobParams<T> = T extends (globs: string | string[], ...params: infer TParams extends unknown[]) => GlobMatcher ? TParams : [];
+export type Formatter = (input: string | number | null | undefined) => string
+
+export interface Colors {
+	isColorSupported: boolean
+
+	reset: Formatter
+	bold: Formatter
+	dim: Formatter
+	italic: Formatter
+	underline: Formatter
+	inverse: Formatter
+	hidden: Formatter
+	strikethrough: Formatter
+
+	black: Formatter
+	red: Formatter
+	green: Formatter
+	yellow: Formatter
+	blue: Formatter
+	magenta: Formatter
+	cyan: Formatter
+	white: Formatter
+	gray: Formatter
+
+	bgBlack: Formatter
+	bgRed: Formatter
+	bgGreen: Formatter
+	bgYellow: Formatter
+	bgBlue: Formatter
+	bgMagenta: Formatter
+	bgCyan: Formatter
+	bgWhite: Formatter
+
+	blackBright: Formatter
+	redBright: Formatter
+	greenBright: Formatter
+	yellowBright: Formatter
+	blueBright: Formatter
+	magentaBright: Formatter
+	cyanBright: Formatter
+	whiteBright: Formatter
+
+	bgBlackBright: Formatter
+	bgRedBright: Formatter
+	bgGreenBright: Formatter
+	bgYellowBright: Formatter
+	bgBlueBright: Formatter
+	bgMagentaBright: Formatter
+	bgCyanBright: Formatter
+	bgWhiteBright: Formatter
+}
